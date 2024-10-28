a20c2a0dd4d93ef6b2788c4219975e70ec5bdf84
Joaquin Gomez <joaquingomez@my.whccd.edu>
2024-10-28 00:17:15 -0700

    FizzBuzz Fizz Buzz

--- script2.js
+++ script2.js
@@ -9,3 +9,15 @@
 // 14
 // FizzBuzz
 // ...
+
+for (let i = 1; i <= 100; i++) { // loop from 1 to 100
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz'); // fizzbuzz will print if i is divisible by both 3 and 5
+    } else if (i % 3 === 0) {
+        console.log('Fizz'); // fizz will print if i is divisible by 3
+    } else if (i % 5 === 0) {
+        console.log('Buzz'); // buzz will print if i is divisible by 5
+    } else {
+        console.log(i); // if i is not divisible by 3 or 5, it will print i
+    }
+}
