8e12a16ecd32c97afc9c5f3e482e4d69dacb1c16
Joaquin Gomez <joaquingomez@my.whccd.edu>
2024-10-28 00:12:22 -0700

    Triangle

--- script1.js
+++ script1.js
@@ -7,3 +7,14 @@
 // ###
 // ##
 // #
+function triangle() {
+    const maxRow = 4; // display 5 rows
+    for (let i = 0; i < maxRow; i++) {
+        console.log('#'.repeat(i + 1));
+    }// Add i is equal to 5 
+    for (let i = maxRow - 1; i >= 0; i--) {
+        console.log('#'.repeat(i));
+    }
+}// Display 5 rows of #
+
+triangle()      
